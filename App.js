f0cff50d171fe317bb93058f7789dfdc2501de5b
Tyler Couto <tyler@gomerits.com>
2022-12-14 11:44:03 -0800

    Add datadog error tracking code

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+import {
+  DdLogs,
+  DdRum,
+  DdSdkReactNative,
+  DdSdkReactNativeConfiguration,
+} from "expo-datadog";
+
+const config = new DdSdkReactNativeConfiguration(
+  "client_token",
+  "local",
+  "application_id",
+  false, // track user interactions such as tapping on a button. You can use
+  false, // track XHR resources.
+  true // track errors.
+);
+
+config.serviceName = "test";
+config.verbosity = "debug";
+
+DdSdkReactNative.initialize(config);
 
 export default function App() {
+  useEffect(() => {
+    DdLogs.info("hello");
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        onPress={() => {
+          const err = new Error("add ahh!");
+          // throw err;
+          DdRum.addError(err.message, "source", err.stack);
+        }}
+      >
+        <Text>Press me!</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +45,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
